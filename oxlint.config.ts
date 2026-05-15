64496eaebd1e9fb3fe67cec5d094ef1c46c31e0b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2026-05-15 09:14:06 +0200

    Configure oxc toolchain

---
+++ oxlint.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "oxlint";
+import core from "ultracite/oxlint/core";
+import next from "ultracite/oxlint/next";
+import react from "ultracite/oxlint/react";
+
+export default defineConfig({
+  extends: [core, react, next],
+});
